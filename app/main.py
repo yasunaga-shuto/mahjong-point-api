5cca6170168f4c0eb31e0720176e2d38570f2e1d
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-09-16 15:54:41 +0900

    cors追加

--- app/main.py
+++ app/main.py
@@ -4,9 +4,20 @@ from mahjong.tile import TilesConverter
 from mahjong.hand_calculating.hand_config import HandConfig, OptionalRules
 from mahjong.meld import Meld
 from mahjong.constants import EAST, SOUTH, WEST, NORTH
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 calculator = HandCalculator()
+origins = [
+  "http://localhost:3000",
+]
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
